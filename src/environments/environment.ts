882f92cf6a38748222b5438b5be38d242c72476d
Ana Raquel de Holanda Barros <anaraquel00@gmail.com>
2026-05-22 09:08:06 -0300

    "fix: hardcode da url do render no environment base para forcar o build da vercel"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:3000/api/v1' // O porto seguro da sua máquina
+  production: true,
+  baseUrl: 'https://rqs-daw-backend.onrender.com/api/v1'
 };
