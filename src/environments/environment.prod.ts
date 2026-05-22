48ed8b1285949c571fedb9318101ab2319b12272
Ana Raquel de Holanda Barros <anaraquel00@gmail.com>
2026-05-22 08:32:44 -0300

    "feat: plugando o motor dsp de producao do render"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://rqs-backend-production.up.railway.app/api/v1'
-  // 👆 Nota: Substituiremos esse link pelo endereço real assim que o Railway gerar!
+  baseUrl: 'https://rqs-daw-backend.onrender.com/api/v1'
 };
