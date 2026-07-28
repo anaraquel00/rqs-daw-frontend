20bb81e7ee2825f102407a84b0f9c9f0c62d00d9
Ana Raquel de Holanda Barros <anaraquel00@gmail.com>
2026-07-28 09:23:29 -0300

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://m2ud3r3gh7vocnc3hzvhnv4s4m0dmujw.lambda-url.sa-east-1.on.aws/'
+  baseUrl: 'https://m2ud3r3gh7vocnc3hzvhnv4s4m0dmujw.lambda-url.sa-east-1.on.aws'
 };
