c9ef9dff68fc3e905ee58eae5eab65c7920d44ec
Kris <migol21@o2.pl>
2026-08-10 23:44:16 +0200

    feat(mastering): separate production v2 environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://m2ud3r3gh7vocnc3hzvhnv4s4m0dmujw.lambda-url.sa-east-1.on.aws'
+  baseUrl: 'https://m2ud3r3gh7vocnc3hzvhnv4s4m0dmujw.lambda-url.sa-east-1.on.aws',
+  masteringV2DirectUpload: false,
 };
